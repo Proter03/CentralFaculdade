44981146ffe5704319fd79a9780d5d57cc28e70f
Cezarine <gcezarines@gmail.com>
2024-09-15 09:58:25 -0300

    Ajustando warning

--- app/src/main/java/com/felipegabriel/centralfaculdade/repository/GenericDatabase.java
+++ app/src/main/java/com/felipegabriel/centralfaculdade/repository/GenericDatabase.java
@@ -17,7 +17,7 @@ public class GenericDatabase<T> extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String COLUMN_USERNAME = "usuario";
     private static final String COLUMN_PASSWORD = "senha";
-    private Class<T> clazz;
+    private final Class<T> clazz;
 
     public GenericDatabase(Context context, Class<T> clazz) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
